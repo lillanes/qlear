d4b9b43744df9c98f859490353e11a58fef6056c
León Illanes <lillanes@cs.toronto.edu>
2019-06-13 10:27:46 -0400

    Use q-learning in grid example

--- examples/grid/grid.cc
+++ examples/grid/grid.cc
@@ -29,6 +29,13 @@ struct Position {
     Position(Position const &) = default;
     Position & operator=(Position const &) = default;
 
+    Position(qlear::State const &id)
+        : x(id % WIDTH), y(id / WIDTH) {}
+
+    qlear::State id() const {
+        return WIDTH * y + x;
+    }
+
     bool operator==(Position const &other) const {
         return x == other.x && y == other.y;
     }
@@ -44,59 +51,64 @@ bool test_success() {
     return (dist(rng) <= SUCCESS_RATE);
 }
 
-void move_if_possible(Position &position, int dx, int dy) {
+Position move_if_possible(Position position, int dx, int dy) {
     if ((dx < 0 && position.x > 0) || (dx > 0 && position.x < WIDTH - 1)) {
         position.x += dx;
     }
     if ((dy < 0 && position.y > 0) || (dy > 0 && position.y < HEIGHT - 1)) {
         position.y += dy;
     }
+    return position;
 }
 
-void up(Position &position) {
+Position up(Position const &position) {
     if (test_success()) {
-        move_if_possible(position, 0, -1);
+        return move_if_possible(position, 0, -1);
     } else {
         switch (choose(rng)) {
-        case 0: move_if_possible(position, 1, 0); break;
-        case 1: move_if_possible(position, 0, 1); break;
-        case 2: move_if_possible(position, -1, 0); break;
+        case 0: return move_if_possible(position, 1, 0);
+        case 1: return move_if_possible(position, 0, 1);
+        case 2: return move_if_possible(position, -1, 0);
+        default: return position;
         }
     }
 }
 
-void right(Position &position) {
+Position right(Position const &position) {
     if (test_success()) {
-        move_if_possible(position, 1, 0);
+        return move_if_possible(position, 1, 0);
     } else {
         switch (choose(rng)) {
-        case 0: move_if_possible(position, 0, 1); break;
-        case 1: move_if_possible(position, -1, 0); break;
-        case 2: move_if_possible(position, 0, -1); break;
+        case 0: return move_if_possible(position, 0, 1);
+        case 1: return move_if_possible(position, -1, 0);
+        case 2: return move_if_possible(position, 0, -1);
+        default: return position;
         }
     }
 }
 
-void down(Position &position) {
+Position down(Position const &position) {
     if (test_success()) {
-        move_if_possible(position, 0, 1);
+        return move_if_possible(position, 0, 1);
     } else {
         switch (choose(rng)) {
-        case 0: move_if_possible(position, -1, 0); break;
-        case 1: move_if_possible(position, 0, -1); break;
-        case 2: move_if_possible(position, 1, 0); break;
+        case 0: return move_if_possible(position, -1, 0);
+        case 1: return move_if_possible(position, 0, -1);
+        case 2: return move_if_possible(position, 1, 0);
+        default: return position;
         }
     }
 }
 
-void left(Position &position) {
+Position left(Position const &position) {
     if (test_success()) {
-        move_if_possible(position, -1, 0);
+        return move_if_possible(position, -1, 0);
     } else {
         switch (choose(rng)) {
-        case 0: move_if_possible(position, 0, -1); break;
-        case 1: move_if_possible(position, 1, 0); break;
-        case 2: move_if_possible(position, 0, 1); break;
+        case 0: return move_if_possible(position, 0, -1);
+        case 1: return move_if_possible(position, 1, 0);
+        case 2: return move_if_possible(position, 0, 1);
+        default: return position;
         }
     }
 }
@@ -120,10 +132,10 @@ int random_agent(Position const &start, Position const &goal,
     int reward = 0;
     for (unsigned int i = 0 ; i < steps; ++i) {
         switch (uniform_action(rng)) {
-        case 0: up(pos); break;
-        case 1: right(pos); break;
-        case 2: down(pos); break;
-        case 3: left(pos); break;
+        case 0: pos = up(pos); break;
+        case 1: pos = right(pos); break;
+        case 2: pos = down(pos); break;
+        case 3: pos = left(pos); break;
         case 4: break; // no-op
         }
         reward += goal_reward(pos, goal);
@@ -135,27 +147,32 @@ int random_agent(Position const &start, Position const &goal,
     return reward;
 }
 
-#if 0
 int qlear_agent(Position const &start, Position const &goal,
         unsigned int steps, bool verbose=false) {
-    Position pos(start);
-
     if (verbose) {
-        std::cout << "From " << pos << " to " << goal << " -- ";
+        std::cout << "From " << start << " to " << goal << "\n";
     }
 
-    qlear::Environment env(pos,
-            {up, right, down, left, [](Position &){}},
-            [&goal](Position const &, size_t, Position const &s1) {
-                return goal_reward(s1, goal);
-            }
-            );
+    auto a_up = [&](qlear::State s){ return up(Position(s)).id(); };
+    auto a_right = [&](qlear::State s){ return right(Position(s)).id(); };
+    auto a_down = [&](qlear::State s){ return down(Position(s)).id(); };
+    auto a_left = [&](qlear::State s){ return left(Position(s)).id(); };
+    auto a_noop = [](qlear::State s){ return s; };
 
-    qlear::train(env, steps, verbose);
-    int eval_reward = qlear::evaluate(env, steps, verbose);
+    std::vector<qlear::Action> actions({a_up, a_right, a_down, a_left, a_noop});
+
+    auto reward = [&](qlear::State, qlear::Action, qlear::State s1) -> double {
+        return goal_reward(Position(s1), goal);
+    };
+
+    qlear::Environment env(start.id(), actions, reward);
+
+    env.train(steps, 0.9, 0.9, verbose);
+
+    int eval_reward = env.evaluate(steps, verbose);
+
     return eval_reward;
 }
-#endif
 
 int main(int const argc, char const **argv) {
     unsigned int experiments = 1000;
@@ -183,12 +200,14 @@ int main(int const argc, char const **argv) {
     std::uniform_int_distribution<> y(0, HEIGHT - 1);
 
     int random_reward = 0;
+    int qlear_reward = 0;
 
     for (unsigned int i = 0; i < experiments; ++i) {
         Position pos(x(rng), y(rng));
         Position goal(x(rng), y(rng));
 
         random_reward += random_agent(pos, goal, steps);
+        qlear_reward += qlear_agent(pos, goal, steps, true);
     }
     std::cout << "Random agent:\n";
     std::cout << "  Total reward accrued: " << random_reward << "\n";
@@ -196,4 +215,10 @@ int main(int const argc, char const **argv) {
         << (double) random_reward / experiments << "\n";
     std::cout << "  Average reward per step: "
         << (double) random_reward / (experiments * steps) << "\n";
+    std::cout << "Qlear agent:\n";
+    std::cout << "  Total reward accrued: " << qlear_reward << "\n";
+    std::cout << "  Average reward per experiment: "
+        << (double) qlear_reward / experiments << "\n";
+    std::cout << "  Average reward per step: "
+        << (double) qlear_reward / (experiments * steps) << "\n";
 }
